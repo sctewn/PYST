0ebe97decf2c5166b30946162afc3411816d7b91
sctewn <114336814+sctewn@users.noreply.github.com>
2023-01-26 17:00:42 -0600

    Update Pyst.py
    Download the sounds folder and put it in the same folder of the project just like the repository, remember to install playsound==1.2.2 library in order to get the answer with a meme sound lmao. You can put all the sounds you want in .wav or .mp3 the code will randomly select a file no matter the number on the folder.

--- Pyst.py
+++ Pyst.py
@@ -8,6 +8,7 @@
 #pip install speechrecognition
 #pip install pyttsx3
 #pip install pyaudio
+#pip install playsound==1.2.2
 
 #Import Libraries
 import speech_recognition as  sr #this will recognize your speech
@@ -15,6 +16,7 @@ import pyttsx3 #This is a library that convert text to speech
 import calendar
 import time
 import csv #librarie to write, read, open and save csv files
+from playsound import playsound
 
 r = sr.Recognizer()
 
@@ -27,7 +29,44 @@ while True:
             print("Say something!")
             audio = r.listen(source)
             
-            print("Ok, you said: " + r.recognize_google(audio, language="en-EN")) #you can change the language replacing en-EN for other like French "fr-FR"
+            if 'no' in r.recognize_google(audio, language="en-EN"): 
+                a = random.choice(os.listdir(r"Sounds/Bad"))
+                print(a)
+                sound = 'Sounds/Bad/'+a
+                playsound(sound)
+            elif 'i will do it' in r.recognize_google(audio, language="en-EN"): 
+                b = random.choice(os.listdir(r"Sounds/Waiting"))
+                print(b)
+                sound = 'Sounds/Waiting/'+b
+                playsound(sound) 
+            elif 'yes i will' in r.recognize_google(audio, language="en-EN"): 
+                c = random.choice(os.listdir(r"Sounds/Waiting"))
+                print(c)
+                sound = 'Sounds/Waiting/'+c
+                playsound(sound) 
+            elif 'wait' in r.recognize_google(audio, language="en-EN"): 
+                d = random.choice(os.listdir(r"Sounds/Waiting"))
+                print(d)
+                sound = 'Sounds/Waiting/'+d
+                playsound(sound) 
+            elif 'hold on' in r.recognize_google(audio, language="en-EN"): 
+                e = random.choice(os.listdir(r"Sounds/Waiting"))
+                print(e)
+                sound = 'Sounds/Waiting/'+e
+                playsound(sound)     
+            elif 'done' in r.recognize_google(audio, language="en-EN"): 
+                f = random.choice(os.listdir(r"Sounds/Good"))
+                print(f)
+                sound = 'Sounds/Good/'+f
+                playsound(sound) 
+            elif 'yes' in r.recognize_google(audio, language="en-EN"): 
+                g = random.choice(os.listdir(r"Sounds/Good"))
+                print(g)
+                sound = 'Sounds/Good/'+g
+                playsound(sound)     
+            else:
+                print("i Didn't get it, please repeat it")
+
     
     except sr.UnknownValueError:
         r = sr.Recognizer()
